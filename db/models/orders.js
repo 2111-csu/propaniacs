3789175af9f3635017a0bdf48c43b5b2a9b4792a
Warren Waterworth <warren.waterworth@gmail.com>
2022-04-26 06:44:35 -0600

    Orders table DB adapters functional

--- db/models/orders.js
+++ db/models/orders.js
@@ -19,6 +19,98 @@ const createOrder = async ({ status, userId }) => {
   }
 };
 
+const getAllOrders = async () => {
+  try {
+    const { rows: orders } = await client.query(`
+      SELECT *
+      FROM orders
+      JOIN order_products ON orders.id="orderId"
+    `);
+
+    return orders;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getOrderByUser = async (id) => {
+  try {
+    const { rows: orders } = await client.query(
+      `
+      SELECT *
+      FROM orders
+      JOIN order_products ON orders.id="orderId"
+      WHERE "userId"=$1
+    `,
+      [id]
+    );
+
+    return orders;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getOrderById = async (id) => {
+  try {
+    const { rows: orders } = await client.query(
+      `
+      SELECT *
+      FROM orders
+      JOIN order_products ON orders.id="orderId"
+      WHERE "orderId"=$1
+    `,
+      [id]
+    );
+
+    return orders;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getOrderByProduct = async (id) => {
+  try {
+    const { rows: orders } = await client.query(
+      `
+      SELECT *
+      FROM orders
+      JOIN order_products ON orders.id="orderId"
+      WHERE "productId"=$1
+    `,
+      [id]
+    );
+
+    return orders;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getCartByUser = async (id) => {
+  try {
+    const { rows: orders } = await client.query(
+      `
+      SELECT *
+      FROM orders
+      JOIN order_products ON orders.id="orderId"
+      WHERE "userId"=$1
+      AND status='created'
+      
+    `,
+      [id]
+    );
+
+    return orders;
+  } catch (error) {
+    throw error;
+  }
+};
 module.exports = {
   createOrder,
+  getAllOrders,
+  getOrderByUser,
+  getOrderById,
+  getOrderByProduct,
+  getCartByUser,
 };
