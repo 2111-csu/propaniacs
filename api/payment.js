a1d8afadbfaf3e543a8796660ce7e34163122754
Warren Waterworth <warren.waterworth@gmail.com>
2022-05-14 12:16:56 -0600

    Striple sending payment success console log

--- api/payment.js
+++ api/payment.js
@@ -1,58 +1,70 @@
 const express = require("express");
 const paymentRouter = express.Router();
-require("dotenv").config()
-const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST)
-const bodyParser = require("body-parser")
+require("dotenv").config();
+const stripe = require("stripe")(
+  "sk_test_51KwrdhDpvRAdcMotakkC1v5353UGpCoCIcFwU4Ez59ivdPZ8Ltlptd2I1qd2rDBHevdhsYtH5I6PP291vk5kQdha00k4XQVrzi"
+);
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const { application } = require("express");
 
-const {
-  updateOrder,
-  cancelOrder,
-} = require("../db/models/orders");
+const { updateOrder, cancelOrder } = require("../db/models/orders");
 
-paymentRouter.use(bodyParser.urlencoded({extended: true}))
-paymentRouter.use(bodyParser.json);
+paymentRouter.use(bodyParser.urlencoded({ extended: true }));
+paymentRouter.use(bodyParser.json());
 
 paymentRouter.use(cors());
 
 // paymentRouter.listen(process.env.PORT )
 
-paymentRouter.get("/", async (req, res, next) => {
+paymentRouter.get("/secret", async (req, res, next) => {
   try {
-    
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "USD",
+      description: "Strick-Land Propane",
+      payment_method: id,
+      confirm: true,
+    });
+    console.log("Payment", payment);
+
+    // res.json({
+    //   message: "Payment Success!",
+    //   success: true,
+    // });
   } catch (error) {
     console.error(error);
   }
 });
 
 paymentRouter.post("/", cors(), async (req, res, next) => {
-  let {amount, id} = req.body;
-  try{
+  let { amount, id } = req.body;
+  console.log("Working?!?!?!?");
+  try {
     const payment = await stripe.paymentIntents.create({
       amount,
       currency: "USD",
       description: "Strick-Land Propane",
       payment_method: id,
-      confirm: true
-    })
+      confirm: true,
+    });
     console.log("Payment", payment);
     res.json({
       message: "Payment Success!",
-      success: true
-    })
+      success: true,
+    });
   } catch (error) {
     console.log("error", error);
     res.json({
       message: "Payment Failed!",
-      success: false
-    })
-    }
-})
+      success: false,
+    });
+  }
+});
 
 paymentRouter.patch("/:orderId", async (req, res, next) => {
   const { orderId } = req.params;
-  const { status } = req.body
+  const { status } = req.body;
 
   try {
     const updatedOrder = await updateOrder({
@@ -61,29 +73,29 @@ paymentRouter.patch("/:orderId", async (req, res, next) => {
     });
     res.send({
       updatedOrder,
-      message: `This order is ${status}`
+      message: `This order is ${status}`,
     });
   } catch (error) {
-    throw(error);
+    throw error;
   }
 });
 
 paymentRouter.delete("/:orderId", async (req, res, next) => {
-    const { orderId } = req.params;
-    const { status } = req.body
-  
-    try {
-      const canceledOrder = await cancelOrder({
-        id: orderId,
-        status,
-      });
-      res.send({
-        canceledOrder,
-        message: `This order is ${status}`
-      });  
-    } catch (error) {
-      throw(error);
-    }
-  });
+  const { orderId } = req.params;
+  const { status } = req.body;
+
+  try {
+    const canceledOrder = await cancelOrder({
+      id: orderId,
+      status,
+    });
+    res.send({
+      canceledOrder,
+      message: `This order is ${status}`,
+    });
+  } catch (error) {
+    throw error;
+  }
+});
 
 module.exports = paymentRouter;
